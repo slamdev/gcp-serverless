47a03aa93002ba2dae7d648c36a13e831c50a64c
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2019-12-28 20:49:38 +0100

    provision SA key

--- etc/builder/docker-entrypoint.sh
+++ etc/builder/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -euo
 
-cat <<< "$SA_KEY" > "/tmp/gcp_sa.json"
+echo "$SA_KEY" > "/tmp/gcp_sa.json"
 export GOOGLE_APPLICATION_CREDENTIALS="/tmp/gcp_sa.json"
 gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
 gcloud config set container/use_application_default_credentials true
